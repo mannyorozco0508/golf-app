d8c6055ffe2ff74d16a15b9af51f1cea31ff7285
mannyorozco0508 <manny.orozco0508@gmail.com>
2026-09-01 18:39:28 -0700

    Update gross_marks_test.js

--- gross_marks_test.js
+++ gross_marks_test.js
@@ -322,7 +322,7 @@ describe('SERVICE WORKER', () => {
     const sw = read('sw.js');
 
     test('CACHE_VERSION moved to v10', () => {
-        assert.match(sw, /const CACHE_VERSION = 'golfapp-v27-kp-terms';/);
+        assert.match(sw, /const CACHE_VERSION = 'golfapp-v28-kp-carry';/);
         assert.ok(!/const CACHE_VERSION = 'golfapp-v12-course-grid';/.test(sw));
     });
 
