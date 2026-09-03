b6db474464a8099183026589e3748f1bce8e2873
mannyorozco0508 <manny.orozco0508@gmail.com>
2026-09-02 17:40:54 -0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.rattlegolf.app',
   appName: 'Rattle Golf',
-  webDir: 'www'
+  webDir: 'www/app'
 };
 
 export default config;
