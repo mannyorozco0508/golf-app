5a61af30884977fa53c66e2ea41757441342ed25
mannyorozco0508 <manny.orozco0508@gmail.com>
2026-09-02 17:15:27 -0700

    Update gross_marks_test.js

--- gross_marks_test.js
+++ gross_marks_test.js
@@ -322,7 +322,7 @@ describe('SERVICE WORKER', () => {
     const sw = read('sw.js');
 
     test('CACHE_VERSION moved to v10', () => {
-        assert.match(sw, /const CACHE_VERSION = 'golfapp-v35-consumer-separation';/);
+        assert.match(sw, /const CACHE_VERSION = 'golfapp-v36-icon-system';/);
         assert.ok(!/const CACHE_VERSION = 'golfapp-v12-course-grid';/.test(sw));
     });
 
